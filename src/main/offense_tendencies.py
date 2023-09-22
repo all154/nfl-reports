dbb0245cfcb29ff9e58e4e98e0afd28b1c61bcb8
Aloisio Valério Jr <aloisio.valeriojr@gmail.com>
2023-09-22 17:34:26 -0300

    docs: pass_rate_by_personnel

--- src/main/offense_tendencies.py
+++ src/main/offense_tendencies.py
@@ -2,9 +2,25 @@ import nfl_data_py as nfl
 import pandas as pd
 import numpy as np
 
-def pass_rate_by_personnel():
+def pass_rate_by_personnel(team, situation, side):
     '''
-    Description:
+        Pass and rush rate split by personnel
+
+        team (string): 3-letter abreviation of NFL team
+
+        situation (string): different situation of a football match
+            'COMP': all plays
+            'OF': open field
+            'RZ': red zone plays only
+            '2M': 2 minute offense
+            '4M': 4 minute offense
+            'CLUT': clutch time (offense only)
+        
+        side (string): 
+            'OFF': offensive personnel
+            'DEF' for defensive personnel
+
+        Returns: a dataframe with pass and rush rates by down, distance and situation
     '''
     pass
 
